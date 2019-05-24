2dd10c363afc51ed8563043cc879cbb9a0313bd0
minh <minhdotcom@gmail.com>
2019-05-25 01:37:06 +0700

    create errorLogin.jsp

--- src/main/java/com/converter/controller/UserController.java
+++ src/main/java/com/converter/controller/UserController.java
@@ -2,11 +2,20 @@ package com.converter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
     @GetMapping ("home")
     public String home () {
         return "home";
+    }
+
+    @PostMapping
+    public String login () {
+        if (true) {
+            return "errorLogin";
+        } else return "userDetails";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/errorLogin.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: minh
+  Date: 5/25/19
+  Time: 1:35 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error Login</title>
+</head>
+<body>
+<h2>Login failed. Please check your account and password.</h2>
+</body>
+</html>
